d8ba5026b38c9c2b0b5f87bbbc1a9ed564b11d55
Sebastian Borggrewe <me@sebastianborggrewe.de>
2013-03-07 19:52:30 +0100

    Update SBScrollNavigation.m
    Changed License

--- SBScrollNavigation/SBScrollNavigation.m
+++ SBScrollNavigation/SBScrollNavigation.m
@@ -1,10 +1,18 @@
-//
-//  UIWeScrollNavigation.m
-//  Stadtmagazin
-//
-//  Created by Sebastian Borggrewe on 19/10/2012.
-//  Copyright (c) 2012 Stadtmagazin GmbH. All rights reserved.
-//
+/* This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Created by Sebastian Borggrewe on 19/10/2012
+*/
 
 #import "SBScrollNavigation.h"
 #import <QuartzCore/QuartzCore.h>
